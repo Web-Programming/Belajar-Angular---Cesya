13bc1b8c073cd20ebddc75c5a30474ccb6a5a8c0
thisnotulala <cesyacatherine_2226240018@mhs.mdp.ac.id>
2024-11-13 20:30:31 +0700

    commit
    commit

--- PAWII_SI52_ANGULAR/belajar_angular/src/app/home/home.component.ts
+++ PAWII_SI52_ANGULAR/belajar_angular/src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  housingLocationList:HousingLocation[] = [
+    {
+    id: 0,
+    name: "Citra Grand City",
+    city: "Palembang",
+    state: "ID",
+    photo: "https://citragrandcity.co.id/wp-content/uploads/2021/05/Calathea.jpg",
+    availableUnits: 12,
+    wifi: true,
+    laundry: true
+    }    
+  ]
+
 
 }
