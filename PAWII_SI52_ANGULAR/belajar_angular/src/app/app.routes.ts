f69d70ac9ef9fd5448459bb9b086b87ae40c5530
thisnotulala <cesyacatherine_2226240018@mhs.mdp.ac.id>
2024-11-20 20:21:02 +0700

    COMMIT
    COMMIT

--- PAWII_SI52_ANGULAR/belajar_angular/src/app/app.routes.ts
+++ PAWII_SI52_ANGULAR/belajar_angular/src/app/app.routes.ts
@@ -2,15 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
 export const routes: Routes = [
-    {
-        path:'',
+    { 
+        path: '', 
         component: HomeComponent,
         title: 'Home Page'
     },
     {
-        path:'details/:id',
+        path: 'details/:id',
         component: DetailsComponent,
         title: 'Details Page'
     }
 ];
-
